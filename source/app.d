21f208de0a615066f7c8faaf98f70569f361ebfe
Stephan Dilly <stephan@extrawurst.org>
2013-11-24 12:50:25 +0100

    support logfile param for stdout and stderr support max run param

--- source/app.d
+++ source/app.d
@@ -1,7 +1,26 @@
 import std.stdio;
 import std.array:join;
 import std.process;
+import std.getopt;
 import core.thread;
+
+struct CmdOptions
+{
+	string stdoutFile;
+	string stderrFile;
+	int max=-1;
+
+	public void Parse(ref string[] _args)
+	{
+		getopt(_args, 
+			"log|l", &stdoutFile,
+			"err|e", &stderrFile,
+			"max|m", &max);
+	}
+
+	@property bool useStdOutFile() const { return stdoutFile.length > 0; }
+	@property bool useStdErrFile() const { return stderrFile.length > 0; }
+}
 
 void main(string[] _args)
 {
@@ -10,9 +29,19 @@ void main(string[] _args)
 	if(_args.length < 2)
 		return;
 
+	CmdOptions options;
+	options.Parse(_args);
+
+	if(options.useStdOutFile)
+		writefln("--- stdout file: %s", options.stdoutFile);
+	if(options.useStdErrFile)
+		writefln("--- stderr file: %s", options.stderrFile);
+	if(options.max > 0)
+		writefln("--- max runs: %s", options.max);
+
 	auto cmdline = _args[1..$].join(" ");
 
-	while(true)
+	while(options.max == -1 || (options.max-- > 0))
 	{
 		writefln("--- Starting: '%s'", cmdline);
 		scope(exit) writefln("--- Process Ended");
@@ -22,17 +51,33 @@ void main(string[] _args)
 
 		auto outThread = new Thread(()
 		{
+			File outStream = stdout;
+
+			if(options.useStdOutFile)
+				outStream = File(options.stdoutFile, "a");
+
 			foreach (line; pipes.stdout.byLine)
 			{
-				writefln("%s",line);
+				if(options.useStdOutFile)
+					outStream.writef("%s",line);
+				else
+					outStream.writefln("%s",line);
 			}
 		});
 
 		auto errThread = new Thread(()
 		{
+			File outStream = stderr;
+
+			if(options.useStdErrFile)
+				outStream = File(options.stderrFile, "a");
+
 			foreach (line; pipes.stderr.byLine)
 			{
-				writefln("ERR: %s",line);
+				if(options.useStdErrFile)
+					outStream.writef("%s",line);
+				else
+					outStream.writefln("ERR: %s",line);
 			}
 		});
 
